b77c3c93b47f29f2ea5b480d2d3c325148ee6ab3
Dmytro Lukianchuk <dmytro.lukianchuk.lead@gmail.com>
2026-01-24 22:34:47 +0100

    Publish site

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/vitaliy-smyrnov/",
+  logLevel: 'error', // Suppress warnings, only show errors
+  server: {
+    open: true, // Automatically open the browser when the dev server starts
+    port: 5173, // Default Vite port (optional, can be changed)
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [
+    react(),
+  ]
+});
