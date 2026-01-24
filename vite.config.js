05e72181d94ee0f731672caa3b6669b422ca5250
Dmytro Lukianchuk <dmytro.lukianchuk.lead@gmail.com>
2026-01-24 22:02:10 +0100

    initial setup for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/vitaliy-smyrnov/",
   logLevel: 'error', // Suppress warnings, only show errors
   server: {
     open: true, // Automatically open the browser when the dev server starts
